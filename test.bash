a1197787ffab27cb69cda2f055a182dcdd0d47ef
KenInaba <jiandaoye293@gmail.com>
2022-10-27 12:18:02 +0900

    change test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+#SPDX-FileCopyrightText:2022 Ken Inaba rightman20020920@yahoo.co,jp
+#SPDX-Licence-Identifire:BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+       res=1	
+}
+res=0
+
+## I/O TEST ##
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ]
-echo $?
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
