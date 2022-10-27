726defa0df8dcb59e925cdef638e7476906b465d
KenInaba <jiandaoye293@gmail.com>
2022-10-27 12:39:57 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 #SPDX-FileCopyrightText:2022 Ken Inaba rightman20020920@yahoo.co,jp
 #SPDX-Licence-Identifire:BSD-3-Clause
 
@@ -11,6 +11,16 @@ res=0
 ## I/O TEST ##
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]	    ||	ng ${LINENO}
+[ "${out}" = "" ] ||	ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+out=$(echo " " | ./plus )
+[ "$?" = 1 ]	    ||	ng ${LINENO}
+[ "${out}" = "" ] ||	ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
