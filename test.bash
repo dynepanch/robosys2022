87226b100adf04bd45dae9a149aaf9104161edf1
KenInaba <jiandaoye293@gmail.com>
2022-11-18 21:43:11 +0900

    testlatest

--- test.bash
+++ test.bash
@@ -10,18 +10,31 @@ res=0
 
 ## I/O TEST ##
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}"="plus=15 product=120" ] || ng ${LINENO}
+[ "$res" = 0 ] && echo No_5 OK
 
-[ "$res" = 0 ] && echo OK
+
+out=$(seq 10 | ./plus)
+[ "${out}"="plus=55 product=3628800" ] || ng ${LINENO}
+[ "$res" = 0 ] && echo No_10 OK
+
+out=$(seq 0 ./plus)
+[ "${put}"="plus=0 product=1" ] || ng ${LINENO}
+[ "$res"= 0 ] && echo No_0 OK
+
 out=$(echo あ | ./plus)
 [ "$?" = 1 ]	    ||	ng ${LINENO}
 [ "${out}" = "" ] ||	ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
+[ "$res" = 0 ] && echo inputあ OK
 out=$(echo " " | ./plus )
 [ "$?" = 1 ]	    ||	ng ${LINENO}
 [ "${out}" = "" ] ||	ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
+[ "$res" = 0 ] && echo input space OK
+
+out=$(echo 三 | ./plus)
+[ "${out}" = "plus=3 product=3"] 
+[ "$res" = 0 ] && echo input三 OK
 
 exit $res
